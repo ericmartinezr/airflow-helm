3aa01f8663d27d1b830ab3b603a3bb79d0b049ab
ericmartinezr <eric.martinez@live.cl>
2026-03-14 11:19:43 -0300

    wip

--- src/dags/ml/iris.py
+++ src/dags/ml/iris.py
@@ -501,13 +501,15 @@ def iris():
                 [
                     [5.1, 3.5, 1.4, 0.2],
                     [6.2, 3.4, 5.4, 2.3],
-                    [5.9, 3.0, 4.2, 1.5]
+                    [5.9, 3.0, 4.2, 1.5],
+                    [0.8, 1.0, 0.2, 0.1]
                 ],
                 columns=[
                     "sepal length (cm)",
                     "sepal width (cm)",
                     "petal length (cm)",
-                    "petal width (cm)"
+                    "petal width (cm)",
+                    "sepal_ratio"
                 ]
             )
             pred = model.predict(df)
